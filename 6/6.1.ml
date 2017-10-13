664e0471d5807eaf5b6a5d1860c760b45f2bb667
921starfish <yuya.921.hoshino@gmail.com>
2017-10-14 02:35:39 +0900

    6.2は他サイトより引用

---
+++ 6/6.1.ml
@@ -0,0 +1,14 @@
+type figure =
+    Point
+  | Circle of int
+  | Rectangle of int * int
+  | Square of int;;
+
+(*以上デバッグ用*)
+
+let similar x y = match (x, y) with
+    (Point, Point) | (Circle _, Circle _) | (Square _, Square _) -> true
+  | (Rectangle (l1, l2), Rectangle (l3, l4)) -> (l3 * l2 - l4 * l1) = 0
+  | (Rectangle (l1, l2), Square _) ->  l1 == l2
+  | (Square _,Rectangle (l1, l2)) ->  l1 == l2
+  | _ -> false;;
