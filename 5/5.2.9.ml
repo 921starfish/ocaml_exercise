a59f8a17f59c0f5bb65e38b36ea7119ce0f66fde
921starfish <yuya.921.hoshino@gmail.com>
2017-10-16 06:23:05 +0900

    5.2.9.mlをリファクタリング

--- 5/5.2.9.ml
+++ 5/5.2.9.ml
@@ -1,5 +1,6 @@
-let rec max_list lst = 
-    match lst with
-    x :: [] -> x
-  | x :: rest -> let y = max_list rest in if x > y then x else y
-  | [] -> [];;
+let max_list lst = 
+    let rec _max_list lst max =
+        match lst with
+      [] -> max
+    | x::rest -> if x > max then _max_list rest x else _max_list rest max
+in _max_list lst min_int;;
