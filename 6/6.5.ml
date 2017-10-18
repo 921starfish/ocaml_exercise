8e3706a19470661f2685d1da794b04ebfa682849
921starfish <yuya.921.hoshino@gmail.com>
2017-10-19 05:33:45 +0900

    木

---
+++ 6/6.5.ml
@@ -0,0 +1,9 @@
+type 'a tree = Lf | Br of 'a * 'a tree * 'a tree;;
+
+let rec comptree x n =
+    if n = 0 then Lf else Br (x, comptree x (n-1), comptree x (n-1));;
+
+let rec comptree' n =
+    let rec _comptree' n i =
+        if n = 0 then Lf else Br (i, _comptree' (n-1) (i*2), _comptree' (n-1) (i*2+1))
+in _comptree' n 1;;
