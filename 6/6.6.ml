e61b0d95aee3090619701ecb076dc8fbe042d795
921starfish <yuya.921.hoshino@gmail.com>
2017-10-19 05:44:25 +0900

    inordと似た感じで機械的に置き換えてみたけど大丈夫みたい

---
+++ 6/6.6.ml
@@ -0,0 +1,18 @@
+type 'a tree = Lf | Br of 'a * 'a tree * 'a tree;;
+
+
+
+let rec preord t l =
+    match t with
+    Lf -> l
+  | Br(x, left, right) -> x :: (preord left (preord right l));;
+
+let rec inord t l =
+    match t with
+    Lf -> l
+  | Br(x, left, right) -> (inord left (x :: inord right l));;
+
+let rec postord t l =
+    match t with
+    Lf -> l
+  | Br(x, left, right) -> (postord left (postord right (x :: l)));;
