cf024f3018380877d2ba48294ee76481fd1d6deb
921starfish <yuya.921.hoshino@gmail.com>
2017-10-24 03:19:14 +0900

    7章終了

--- 7/7.2.ml
+++ 7/7.2.ml
@@ -4,4 +4,4 @@ let rec prod_list lst =
   let rec prod_list' lst results = match lst with
       [] -> results
     | a::rest -> if a = 0 then raise IsZero else prod_list' rest a*results
-  in try (prod_list' lst 1) with IsZero -> 0;;
+  in try (prod_list' lst 1) with IsZero -> 0
