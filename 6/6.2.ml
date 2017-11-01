4cf3f488bf289fec1ef180b7890ba3065599f272
921starfish <yuya.921.hoshino@gmail.com>
2017-11-02 02:45:51 +0900

    6.2.ml あんまりOCamlの練習した気がしない問題

--- 6/6.2.ml
+++ 6/6.2.ml
@@ -2,116 +2,62 @@ type figure =
     Point
   | Circle of int
   | Rectangle of int * int
-  | Square of int;;
+  | Square of int
 
-type 'a with_location = {loc_x: float; loc_y: float; body: 'a};;
+type 'a with_location = {loc_x: float; loc_y: float; body: 'a}
 
 (*以上デバッグ用*)
 
-let overlap_point_point f1 f2 =
-  (f1.loc_x = f2.loc_x) && (f1.loc_y = f2.loc_y);;
-let overlap_point_circle f1 f2 =
-  let Circle r = f2.body in
-  (f2.loc_x -. f1.loc_x) ** 2.0 +. (f2.loc_y -. f1.loc_y) ** 2.0 <= float_of_int (r * r);;
+let point_of_rectangle fig = 
+  match fig.body with 
+    Rectangle (x,y)->
+    ({loc_x=fig.loc_x -. float x /. 2.; loc_y= fig.loc_y -. float y /. 2.;body=Point},
+     {loc_x=fig.loc_x -. float x /. 2.; loc_y= fig.loc_y +. float y /. 2.;body=Point},
+     {loc_x=fig.loc_x +. float x /. 2.; loc_y= fig.loc_y -. float y /. 2.;body=Point},
+     {loc_x=fig.loc_x +. float x /. 2.; loc_y= fig.loc_y +. float y /. 2.;body=Point})
+  | _ ->raise (Sys_error "到達不能箇所_")
 
-let overlap_point_rectangle f1 f2 =
-    let {loc_x = loc_x; loc_y = loc_y; body = Rectangle (x, y)} = f2 in
-    (loc_x -. float_of_int(x) /. 2.0 <= f1.loc_x) &&
-      (loc_x +. float_of_int(x) /. 2.0 >= f1.loc_x) &&
-      (loc_y -. float_of_int(y) /. 2.0 <= f1.loc_y) &&
-      (loc_y +. float_of_int(y) /. 2.0 >= f1.loc_y);;
+let overlap_point fig1 fig2 =
+  match fig2.body with
+    Point -> (fig1.loc_x = fig2.loc_x) && (fig1.loc_y = fig2.loc_y)
+  | Circle r -> (fig2.loc_x -. fig1.loc_x) ** 2.0 +. (fig2.loc_y -. fig1.loc_y) ** 2.0 <= float_of_int (r * r)
+  | Rectangle (x,y) ->  (fig2.loc_x -. float x /. 2.0 <= fig1.loc_x) &&
+                        (fig2.loc_x +. float x /. 2.0 >= fig1.loc_x) &&
+                        (fig2.loc_y -. float y /. 2.0 <= fig1.loc_y) &&
+                        (fig2.loc_y +. float y /. 2.0 >= fig1.loc_y)
+  | Square z -> raise (Sys_error "到達不能箇所P")
 
-let overlap_circle_circle f1 f2 =
-  match (f1.body, f2.body) with
-      (Circle r1, Circle r2) ->
-  (f1.loc_x -. f2.loc_x) ** 2.0 +. (f1.loc_y -. f2.loc_y) ** 2.0 <= float_of_int (2 * (r1 + r2))
-    | (_, _) -> false;;
+let overlap_circle fig1 fig2 =
+  match (fig1.body,fig2.body) with
+    (Circle r1,Circle r2) -> (fig1.loc_x -. fig2.loc_x) ** 2.0 +. (fig1.loc_y -. fig2.loc_y) ** 2.0 <= float_of_int (2 * (r1 + r2))
+  | (Circle r,Rectangle (x,y)) -> 
+    let (c1, c2, c3, c4) = point_of_rectangle fig2 
+    and rr = {loc_x=fig1.loc_x; loc_y=fig1.loc_y; body=Point} in
+    overlap_point c1 fig1 || overlap_point c2 fig1 || overlap_point c3 fig1 || overlap_point c4 fig1 ||
+    overlap_point rr fig2
+  | _ -> raise (Sys_error "到達不能箇所C")
 
-let corners_of_rectangle f =
-  let {loc_x = rx; loc_y = ry; body = Rectangle (x, y)} = f in
-  ((rx -. (float_of_int x) /. 2.0, ry -. (float_of_int y) /. 2.0),
-   (rx -. (float_of_int x) /. 2.0, ry +. (float_of_int y) /. 2.0),
-   (rx +. (float_of_int x) /. 2.0, ry -. (float_of_int y) /. 2.0),
-   (rx +. (float_of_int x) /. 2.0, ry +. (float_of_int y) /. 2.0));;
+let overlap_rectangle fig1 fig2 =
+  match (fig1.body,fig2.body) with
+  | (Rectangle (x1,y1),Rectangle (x2,y2)) -> 
+    let (c11, c12, c13, c14) = point_of_rectangle fig1 
+    and (c21, c22, c23, c24) = point_of_rectangle fig2 in
+    overlap_point c11 fig2 || overlap_point c12 fig2 || overlap_point c13 fig2 || overlap_point c14 fig2 ||
+    overlap_point c21 fig1 || overlap_point c22 fig1 || overlap_point c23 fig1 || overlap_point c24 fig1 
+  | _ -> raise (Sys_error "到達不能箇所R")
 
-let in_circle point cir =
-  let px = fst point in
-  let py = snd point in
-  let { loc_x = cx; loc_y = cy; body = Circle r} = cir in
-  (cx -. px) ** 2.0 +. (cy -. py) ** 2.0 <= float_of_int(r * r);;
-
-let in_rectangle point rect =
-  let { loc_x = lx; loc_y = ly; body = Rectangle(x,y) } = rect in
-  (fst point) >= (lx -. (float_of_int x) /. 2.0) &&
-    (fst point) <= (lx +. (float_of_int x) /. 2.0) &&
-    (snd point) >= (ly -. (float_of_int y) /. 2.0) &&
-    (snd point) <= (ly +. (float_of_int y) /. 2.0);;
-
-let overlap_circle_rectangle f1 f2 =
-  let {loc_x = cx; loc_y = cy; body = Circle r} = f1 in
-  let (c1, c2, c3, c4) = corners_of_rectangle f2 in
-  in_circle c1 f1 || in_circle c2 f1 || in_circle c3 f1 || in_circle c4 f1 ||
-    in_rectangle (cx -. float_of_int r, cy) f2 ||
-    in_rectangle (cx +. float_of_int r, cy) f2 ||
-    in_rectangle (cx, cy -. float_of_int r) f2 ||
-    in_rectangle (cx, cy +. float_of_int r) f2;;
-
-
-let overlap_rectangle_rectangle f1 f2 =
-  let {loc_x = rx1; loc_y = ry1; body = Rectangle (x1, y1)} = f1 in
-  let {loc_x = rx2; loc_y = ry2; body = Rectangle (x2, y2)} = f2 in
-  if x1 * y1 < x2 * y2 then
-    let (c1, c2, c3, c4) = corners_of_rectangle f1 in
-    in_rectangle c1 f2 || in_rectangle c2 f2 || in_rectangle c3 f2 || in_rectangle c4 f2
-  else
-    let (c1, c2, c3, c4) = corners_of_rectangle f2 in
-    in_rectangle c1 f1 || in_rectangle c2 f1 || in_rectangle c3 f1 || in_rectangle c4 f1;;
-
-let rectangle_of_square f =
-  match f.body with
-      Square x' ->
-  {f with body = Rectangle (x',x')}
-    | _ ->
-      f;;
-
-let overlap f1 f2 =
-  match (f1.body) with
-      Point ->
-  (match f2.body with
-      Point ->
-        overlap_point_point f1 f2
-    | Circle r ->
-      overlap_point_circle f1 f2
-    | Square x ->
-      overlap_point_rectangle f1 (rectangle_of_square f2)
-    | Rectangle (x, y) ->
-      overlap_point_rectangle f1 f2
-  )
-    | Circle r ->
-      (match f2.body with
-    Point ->
-      overlap_point_circle f2 f1
-  | Circle r' ->
-    overlap_circle_circle f1 f2
-  | Rectangle(x, y) ->
-    overlap_circle_rectangle f1 f2
-  | Square x ->
-    overlap_circle_rectangle f1 (rectangle_of_square f2)
-      )
-    | Rectangle (_, _) | Square _ ->
-      (match f2.body with
-    Point ->
-      overlap_point_rectangle f2 (rectangle_of_square f1)
-  | Circle r' ->
-    overlap_circle_rectangle f2 (rectangle_of_square f1)
-  | Rectangle (x', y') ->
-    overlap_rectangle_rectangle (rectangle_of_square f1) (rectangle_of_square f2)
-  | Square x' ->
-    overlap_rectangle_rectangle (rectangle_of_square f1) (rectangle_of_square f2)
-      )
-;;
-
-
-
-
-
+let overlap fig1 fig2 = 
+  match (fig1.body,fig2.body) with
+    (Point,Square z) -> overlap_point fig1 {loc_x=fig2.loc_x; loc_y=fig2.loc_y; body=Rectangle(z,z)}
+  | (Point,_) -> overlap_point fig1 fig2 
+  | (Circle _,Point) -> overlap_point fig2 fig1 
+  | (Circle _,_) -> overlap_circle fig1 fig2
+  | (Rectangle _,Point) -> overlap_point fig2 fig1
+  | (Rectangle _,Circle _) -> overlap_circle fig2 fig1
+  | (Rectangle _,Square z) -> overlap_rectangle fig1 {loc_x=fig2.loc_x; loc_y=fig2.loc_y; body=Rectangle(z,z)}
+  | (Rectangle _,_) -> overlap_rectangle fig1 fig2
+  | (Square z,Point) -> overlap_point fig2 {loc_x=fig2.loc_x; loc_y=fig2.loc_y; body=Rectangle(z,z)} 
+  | (Square z,Circle _) -> overlap_circle fig2 {loc_x=fig2.loc_x; loc_y=fig2.loc_y; body=Rectangle(z,z)}
+  | (Square z,Rectangle _) -> overlap_rectangle fig2 {loc_x=fig2.loc_x; loc_y=fig2.loc_y; body=Rectangle(z,z)}
+  | (Square z1,Square z2) -> overlap_rectangle {loc_x=fig1.loc_x; loc_y=fig1.loc_y; body=Rectangle(z1,z1)} 
+                               {loc_x=fig2.loc_x; loc_y=fig2.loc_y; body=Rectangle(z2,z2)}
